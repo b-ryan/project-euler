0770e73f4b12559ef84d7de3660e97965c4b16b5
Buck Ryan <buck.ryan@gmail.com>
2016-08-20 15:40:46 -0400

    finalize 32

--- problem32.hs
+++ problem32.hs
@@ -1,5 +1,5 @@
-import Data.List (sort)
-pandigital x y = (sort $ (show x) ++ (show y) ++ (show $ x * y)) == "123456789"
+import Data.List (nub, sort)
+pandigital x y =  (== "123456789") . sort $ (show x) ++ (show y) ++ (show $ x * y)
 one = [x * y | x <- [2..9], y <- [1000..9999], pandigital x y]
 two = [x * y | x <- [10..99], y <- [100..999], pandigital x y]
-main = putStrLn $ show $ sort $ one ++ two
+main = putStrLn $ show $ sum $ nub $ one ++ two
